d83493cbfa6cfb5c6785411d95b805eb170631cb
AbdulAidil <abdulaidil2007@gmail.com>
2025-08-11 13:08:59 +0800

    Lab test 2

---
+++ labs/lab-test-2.py
@@ -0,0 +1,27 @@
+#Programmer's name : Abdul Aidil Shah Bin Ajibir
+#Problem description : Complete a python program that uses an appropriate escape character and arithmetic expression.
+
+name_1 = "Name: Ahmad Bin Abu\t\t\tMatric. No: MS2025123499"
+
+symbol_1 = "*\t\t\t*"
+symbol_2 = "**\t\t**"
+symbol_3 = "***\t***"
+symbol_4 = "*******"
+symbol_5 = "***\t***"
+symbol_6 = "**\t\t**"
+symbol_7 = "*\t\t\t*"
+
+full_marks = 2 * 10
+name_2 = "This is my\n\tsecond\n\t\tassignment"
+
+print(name_1)
+print(symbol_1)
+print(symbol_2)
+print(symbol_3)
+print(symbol_4)
+print(symbol_5)
+print(symbol_6)
+print(symbol_7)
+print(name_2)
+
+print("I want 2x10 marks, which is" ,full_marks ,"full marks")
